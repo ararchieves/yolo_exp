d21331fa88d8af696a51a7dc8d034869c85e0fb8
ararchieves <abdulraufs8788@gmail.com>
2024-02-13 20:23:49 +0500

    Add download_data fn in utils

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,7 @@
 import gdown
 
-def download_data(link="    ")
+DATA_URL = "https://drive.google.com/file/d/1EpDh7MyHzXbHNw_FYhbFijYipr2evc01/view?usp=sharing"
+
+
+def download_data(url=DATA_URL):
+    gdown.download(url, 'dataset.zip', fuzzy=True)
