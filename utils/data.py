d3445e4d5be112c2495eae84c6386a8e120fdb39
ararchieves <abdulraufs8788@gmail.com>
2024-02-14 19:40:38 +0500

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -2,10 +2,18 @@ import os
 import gdown
 import shutil
 
-DATA_URL = "https://drive.google.com/file/d/1EpDh7MyHzXbHNw_FYhbFijYipr2evc01/view?usp=sharing"
 
+INRIA_DATA_URL = "https://drive.google.com/file/d/1EpDh7MyHzXbHNw_FYhbFijYipr2evc01/view?usp=sharing"
+DHA_DATA_URL = "https://drive.google.com/file/d/1-uLZZ37H9U9QhTgB3Lj6dXsM1nXfds8z/view?usp=sharing"
 
-def download_dataset(url=DATA_URL):
+def download_dataset(data='inria'):
+    assert data in ['inria', 'dha'], f"Dataset {data} is not valid."
+
+    if data == 'inria': url = INRIA_DATA_URL
+    elif data == 'dha': url = DHA_DATA_URL
+
     gdown.download(url, 'dataset.zip', fuzzy=True) # Donwload Dataset
+    print("Unzipping!")
     shutil.unpack_archive('dataset.zip', '.') # unzip dataset
     os.remove('dataset.zip') # Remove the dataset file
+    print("Setup Complete!")
