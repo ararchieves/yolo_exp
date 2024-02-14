9cfad4674ff6d64d87555ce8a4241fe2d30d28ae
ararchieves <abdulraufs8788@gmail.com>
2024-02-14 15:33:32 +0500

    Update download_dataset fn in utils.data

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,11 @@
+import os
 import gdown
+import shutil
 
 DATA_URL = "https://drive.google.com/file/d/1EpDh7MyHzXbHNw_FYhbFijYipr2evc01/view?usp=sharing"
 
 
-def download_data(url=DATA_URL):
-    gdown.download(url, 'dataset.zip', fuzzy=True)
+def download_dataset(url=DATA_URL):
+    gdown.download(url, 'dataset.zip', fuzzy=True) # Donwload Dataset
+    shutil.unpack_archive('dataset.zip', '.') # unzip dataset
+    os.remove('dataset.zip') # Remove the dataset file
